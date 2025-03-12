255f37e9bf695bf33a6996bef593f0f3d18c1ce5
Aiskiub <danilq139@gmail.com>
2025-03-12 18:06:43 -0500

    feat(frontend): enhance Tailwind configuration with construction theme colors and add dashboard route

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { LoginPage, Register, ForgotPassword as FPassword } from "@/features/auth"
+import Dashboard from "@/components/dashboard/dashboard"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         {/* Otras rutas */}
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<FPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
