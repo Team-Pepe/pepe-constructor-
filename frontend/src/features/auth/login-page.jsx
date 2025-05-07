1f4ff1554a3c8c579909d2eba1fb520125bf95e5
Aiskiub <danilq139@gmail.com>
2025-05-07 18:16:17 -0500

    test

--- frontend/src/features/auth/login-page.jsx
+++ frontend/src/features/auth/login-page.jsx
@@ -53,7 +53,7 @@ export default function LoginPage() {
 
         // Guardar el token de autenticación
         if (response.data.token) {
-          document.cookie = `token=${response.data.token}; path=/`;
+          document.cookie = `token=${response.data.token}; path=/; max-age=3600; SameSite=Lax`;
           localStorage.setItem("authToken", response.data.token); // <-- Agrega esta línea
         }
 
