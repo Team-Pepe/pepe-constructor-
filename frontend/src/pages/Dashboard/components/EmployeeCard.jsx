b3fb3f5570ba576b3517892606d3066a4e860cc2
Juan Esteban <jejaramillocano@gmail.com>
2025-05-13 19:06:22 -0500

    feat(Carnets): se agrego un codigo de barras a cada carnet con el valor del numero de documento

--- frontend/src/pages/Dashboard/components/EmployeeCard.jsx
+++ frontend/src/pages/Dashboard/components/EmployeeCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import Barcode from 'react-barcode';
 import pepeJefeObra from "../../../assets/pepeJefeObra.png"; // Importa la imagen desde la ruta especificada
 
 export function EmployeeCard({ name, id, role, bloodType }) {
@@ -38,9 +39,15 @@ export function EmployeeCard({ name, id, role, bloodType }) {
                 
                 {/* Footer del carnet */}
                 <div className="mt-3 pt-3 border-t border-gray-300">
-                    <div className="flex justify-between">
-                        <div className="text-xs text-gray-500">ID: PPC-JOB-{Math.floor(Math.random() * 1000)}</div>
-                        <div className="text-xs text-gray-500">Válido: 2025-2026</div>
+                    <div className="flex justify-center">
+                        <Barcode 
+                            value={`PPC-${id}`}
+                            width={1.5}
+                            height={40}
+                            fontSize={12}
+                            margin={0}
+                            displayValue={true}
+                        />
                     </div>
                 </div>
             </div>
